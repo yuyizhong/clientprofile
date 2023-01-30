754a88b2c0b52d1f3129bdde59bb71888043c8cd
yuyizhong <zoe_yyz@hotmail.com>
2023-01-30 16:42:52 +0000

    Add comments for imports and scope

--- run.py
+++ run.py
@@ -1,15 +1,23 @@
+"""Client Profile Mgmt Imports"""
+# import gspread to access and edit the excelsheet
 import gspread
+
+# import google oauth to authorize the access to client-Book through credentials
 from google.oauth2.service_account import Credentials
+
+# import tabulate to print the data list in a nice table format
 from tabulate import tabulate
+
 # import pyfiglet module for ascii art
 import pyfiglet
 
-# import colorama for adding colour
+# import colorama for colour text
 import colorama
 from colorama import Fore, Back, Style 
 colorama.init(autoreset=True)
 
-
+# The SCOPE lists the APIs to access to run programme.
+# Set up constant variables
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
