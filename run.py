3fbca8da15c3763a6523787aa20ec02927016142
yuyizhong <zoe_yyz@hotmail.com>
2023-01-31 00:23:41 +0000

    Add function of add new client

--- run.py
+++ run.py
@@ -42,7 +42,8 @@ SHEET = GSPREAD_CLIENT.open('Client-Book')
 # print(type(dob))
 
 def add_client(list):
-    # 1.Add New Client
+    """1.Add New Client"""
+    # Get Client's name and D.o.B
     fname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}First Name{Style.RESET_ALL}:\n").capitalize()
     lname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Last Name{Style.RESET_ALL}:\n").capitalize()
     dob = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Date of Birth formatted as {Fore.YELLOW}dd/mm/yyyy{Style.RESET_ALL}:\n")
@@ -54,16 +55,40 @@ def add_client(list):
             print(f"{Back.RED}{Fore.WHITE}Client is exist! {Style.BRIGHT}Please enter again!!{Style.RESET_ALL}\n")
             exist = True
             break
- 
+    # If not in the list, get client contact number, email and spend
+    # If spend enter is valid then update client Type accordingly   
     if not exist:
         tel = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Contact Number{Style.RESET_ALL}:\n")
         email = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Email{Style.RESET_ALL}:\n")
-        spend = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}New Spend in Sterling Pounds{Style.RESET_ALL}:\n")
-        
-        singlelist = [name, phone]
-        # 将一个人信息组成的列表，添加到总体的列表当中
-        contactlist.append(singlelist)
-        print("输入完成")
+        while True:
+            spend = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}client's spend amount in Sterling Pounds{Style.RESET_ALL}:\n")
+            if validate_spend(spend):
+                break        
+        return spend
 
-    print(fname)
-add_client(list)
+        new_client = [fname, lname, dob, tel, email, spend]
+        # Add the list of new client's information to the clients sheet
+        list.append(new_client)
+        print(f"{Back.GREEN}{Fore.WHITE}client{fname}{lname} is now added to the Client Book.\n")
+
+def validate_spend(value):
+    """
+    Inside the try, converts all string values into floats.
+    And add the float to the previous total spend
+    Raises ValueError if strings cannot be converted into float.
+    
+    """
+
+    try:
+        [float(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values are required, you provided {len(values)}")
+
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again\n")
+        return False
+
+    return True
+
+
