2a9abc88aab985bb4c1bf356842565e5a4dd7e65
yuyizhong <zoe_yyz@hotmail.com>
2023-02-05 02:25:24 +0000

    Add and amend operation and main function

--- run.py
+++ run.py
@@ -5,6 +5,9 @@ import gspread
 # import google oauth to authorize the access to client-Book through credentials
 from google.oauth2.service_account import Credentials
 
+# import os to clear terminal when user runs a new programme
+import os
+
 # import tabulate to print the data list in a nice table format
 from tabulate import tabulate
 
@@ -32,25 +35,51 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Client-Book')
 
-clients = SHEET.worksheet('clients')
+def clear():
+    """
+    Clear screen when user running a new programme
+    """
+    os.system("clear")
 
-def get_initial_data():
-    # Get Client's name and D.o.B
-    fname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}First Name{Style.RESET_ALL}:\n").capitalize()
-    lname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Last Name{Style.RESET_ALL}:\n").capitalize()
-    dob = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Date of Birth formatted as {Fore.YELLOW}dd/mm/yyyy{Style.RESET_ALL}:\n")
-    return fname, lname, dob
+def validate_spend(value):
+    """
+    Inside the try, converts value into float.
+    Raises ValueError if string cannot be converted into float.    
+    """
+    try:
+        spend_num = float(value)
+        print(f"{Back.GREEN}{Fore.BLACK}Number entered is valid.")
+    except ValueError as e:
+        print(f"{Back.RED}{Fore.WHITE}Not a number: {e}, please try again\n")
+        return False
 
+    return True
+
+
+def check_client(worksheet, fname, lname, dob, list):
+
+    """Get user input to check if client is exist"""
+   
+    exist = False  
+    for index in range(len(list)):
+        if (list[index][0] == fname and list[index][1] == lname and list[index][2] == dob):
+            print(f"{Back.RED}{Fore.WHITE}Client {fname} {lname} is in the system{Style.RESET_ALL}\n")
+            
+            return True, index
+            break
+    
+    if not exist:
+        print (f"{Back.RED}{Fore.WHITE}Client {fname} {lname} is not in the system{Style.RESET_ALL}\n")
+        return False
 
 def add_client(worksheet):
 
     """1.Add New Client"""
-
+    
     # Get Client's name and D.o.B    
-    initial_data = get_initial_data()
-    fname=initial_data[0]
-    lname=initial_data[1]
-    dob=initial_data[2]
+    fname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}First Name{Style.RESET_ALL}:\n").capitalize()
+    lname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Last Name{Style.RESET_ALL}:\n").capitalize()
+    dob = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Date of Birth formatted as {Fore.YELLOW}dd/mm/yyyy{Style.RESET_ALL}:\n")
     list = worksheet.get_all_values()
 
     # Check if Client is already in the list.    
@@ -75,54 +104,44 @@ def add_client(worksheet):
         worksheet.append_row(new_client)        
         print(f"{Back.YELLOW}{Fore.BLACK}client {fname} {lname} is now added to the Client Book.\n")
 
-def validate_spend(value):
-    """
-    Inside the try, converts value into float.
-    Raises ValueError if string cannot be converted into float.
+def search_clients(worksheet):
+
+    """2. Search clients and display all the information"""
     
-    """
-    try:
-        spend_num = float(value)
-        print(f"{Back.GREEN}{Fore.BLACK}Number entered is valid.")
-    except ValueError as e:
-        print(f"{Back.RED}{Fore.WHITE}Not a number: {e}, please try again\n")
-        return False
+    # Get user to input client's name
+    print(f"{Fore.CYAN}Provide the below information for the client which you want to search{Fore.RESET}:\n")
+    fname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}First Name{Style.RESET_ALL}:\n").capitalize()
+    lname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Last Name{Style.RESET_ALL}:\n").capitalize()    
+    list = worksheet.get_all_values()
 
-    return True
-
-
-def check_client(worksheet, fname, lname, dob, list):
-
-    """Get user input to check if client is exist"""
-   
-    exist = False  
-    for index in range(len(list)):
-        if (list[index][0] == fname and list[index][1] == lname and list[index][2] == dob):
-            print(f"{Back.RED}{Fore.WHITE}Client alreay exist!{Style.RESET_ALL}\n")
+    # Search client, if exist display the information, otherwise print error message 
+    client_exist = False    
+    client_info = []    
+    for index in range(len(list)):                   
+        if fname == list[index][0] and lname == list[index][1]:            
+            client_info.append(list[index])             
+            client_exist = True   
             
-            return True, index
-            break
+    if not client_exist:
+        print (f"{Back.RED}{Fore.WHITE}Client {fname} {lname} you searched is not fund in the system!{Style.RESET_ALL}\n")
+
+    table = tabulate(client_info, tablefmt="grid")            
+    print(f"{Back.RED}{table}")
+     
+
+def delete_client(worksheet): 
+
+    """3. Delete client"""
     
-    if not exist:
-        print (f"{Back.RED}{Fore.WHITE}Client dose not exist!{Style.RESET_ALL}\n")
-        return False
-        
-        
-
-def delete_client(worksheet, fname, lname, dob): 
-
-    """2. Delete client"""
-
-    # Get user to input client's name and D.o.B    
-    # initial_data = get_initial_data()
-    # fname=initial_data[0]
-    # lname=initial_data[1]
-    # dob=initial_data[2]
+    # Get user to input client's name and D.o.B
+    print(f"{Fore.CYAN}Provide the below information for the client which you want to delete{Fore.RESET}:\n")
+    fname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}First Name{Style.RESET_ALL}:\n").capitalize()
+    lname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Last Name{Style.RESET_ALL}:\n").capitalize()
+    dob = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Date of Birth formatted as {Fore.YELLOW}dd/mm/yyyy{Style.RESET_ALL}:\n")
     list = worksheet.get_all_values()
 
     # Search client, confirm delete decision and delete client, otherwise print error message
     check_results = check_client(worksheet, fname, lname, dob, list)
-    
     
     if check_results[0] == True:          
     
@@ -145,14 +164,39 @@ def delete_client(worksheet, fname, lname, dob):
     if not check_results[0]:
         print (f"{Back.RED}{Fore.WHITE}Client is not exist!{Style.RESET_ALL}\n")
 
+def update_options(worksheet, fname, lname, index, num):
 
+    """ options for user to choose when updating client's basic information"""
+    list = worksheet.get_all_values()
+    option = list[0][num-1] 
+            
+    edit_choice = input(f"{Fore.YELLOW}Do you want to edit client {fname} {lname}'s {option}? Y or N{Fore.RESET}:\n").lower()
+            
+    if edit_choice == "y":
+        new_data = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}updated {option} {Fore.YELLOW}(date please formatted as dd/mm/yyyy){Style.RESET_ALL}:\n")
+        row = index + 1
+        if num == 1 or 2:
+            worksheet.update_cell(row,num, new_data.capitalize())
+        else:
+            worksheet.update_cell(row,num, new_data)
+
+        print(f"{Back.YELLOW}{Fore.BLACK}client's {option} is now updated as {new_data} at Client Book.\n")
+                
+            
+    elif edit_choice == "n":
+        print(f"{Back.GREEN}{Fore.BLACK}You select N, move to next option")
+                
+            
+    else:
+        print(f"{Back.RED}{Fore.WHITE}Not a valid input, please try again!\n")
+        update_client(worksheet)
 
 def update_client(worksheet):
 
-    """3. Update client personal information"""
-
+    """4. Update client personal information"""
+    
     # Get user to input client's name and D.o.B
-    print(f"{Fore.CYAN}Provide the below information for client you want to update{Fore.RESET}:\n")
+    print(f"{Fore.CYAN}Provide the below information for client which you want to update{Fore.RESET}:\n")
     fname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}First Name{Style.RESET_ALL}:\n").capitalize()
     lname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Last Name{Style.RESET_ALL}:\n").capitalize()
     dob = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Date of Birth formatted as {Fore.YELLOW}dd/mm/yyyy{Style.RESET_ALL}:\n")
@@ -206,40 +250,13 @@ def update_client(worksheet):
         print (f"{Back.RED}{Fore.WHITE}Client is not exist!{Style.RESET_ALL}\n")
 
 
-def update_options(worksheet, fname, lname, index, num):
-
-    """ options for user to choose when updating client's basic information"""
-    list = worksheet.get_all_values()
-    option = list[0][num-1] 
-            
-    edit_choice = input(f"{Fore.YELLOW}Do you want to edit client {fname} {lname}'s {option}? Y or N{Fore.RESET}:\n").lower()
-            
-    if edit_choice == "y":
-        new_data = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}updated {option} {Fore.YELLOW}(date please formatted as dd/mm/yyyy){Style.RESET_ALL}:\n")
-        row = index + 1
-        if num == 1 or 2:
-            worksheet.update_cell(row,num, new_data.capitalize())
-        else:
-            worksheet.update_cell(row,num, new_data)
-
-        print(f"{Back.YELLOW}{Fore.BLACK}client's {option} is now updated as {new_data} at Client Book.\n")
-                
-            
-    elif edit_choice == "n":
-        print(f"{Back.GREEN}{Fore.BLACK}You select N, move to next option")
-                
-            
-    else:
-        print(f"{Back.RED}{Fore.WHITE}Not a valid input, please try again!\n")
-        update_client(worksheet)
-
-
 def get_all_clients(worksheet):
 
-    """4. List all the clients' details"""
+    """5. List all the clients' details"""    
     # Option to list Regular clients
     # Option to list Vip clients
     # Option to list All clients
+
     status = input(f"{Fore.YELLOW}Please choose the status of the clients you like to view? Enter Regular, Vip or All{Fore.RESET}:\n").capitalize()
     list = worksheet.get_all_values()        
     
@@ -262,45 +279,24 @@ def get_all_clients(worksheet):
     else:
         print(f"{Back.RED}{Fore.WHITE}Not a valid input, please try again!\n")
         get_all_clients(worksheet)
-  
-
-def main(worksheet):
-    """Run all the management functions"""
-    initial_data = get_initial_data()
-    fname=initial_data[0]
-    lname=initial_data[1]
-    dob=initial_data[2]
-    delete_client(worksheet, fname, lname, dob)
-
-
-
-def search_clients(worksheet):
-    """5. Search clients and display all the information"""
-    # Get user to input client's name
-    print(f"{Fore.CYAN}Provide the below information for client you want to search{Fore.RESET}:\n")
-    fname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}First Name{Style.RESET_ALL}:\n").capitalize()
-    lname = input(f"{Fore.CYAN}Please enter {Style.BRIGHT}Last Name{Style.RESET_ALL}:\n").capitalize()    
-    list = worksheet.get_all_values()
-
-    # Search client, if exist display the information, otherwise print error message 
-    client_exist = False    
-    client_info = []    
-    for index in range(len(list)):            
-        if fname == list[index][0] and lname == list[index][1]:
-            print("-------------------")
-            client_info.append(list[index])                 
-            table = tabulate(client_info, tablefmt="grid")
-            
-            print(f"{Back.RED}{table}")
-            print("-------------------") 
-            client_exist = True
-    if not client_exist:
-        print (f"{Back.RED}{Fore.WHITE}Client is not exist!{Style.RESET_ALL}\n")
     
 def operation (worksheet):
     """link the management functions to the operation menu"""
-    while True:
-        option = input("Please choose one of the options from the above list by entering the corresponding number next to it:")
+    while True:        
+        title = pyfiglet.figlet_format("Clients Management System", font="doom", justify="center")
+        print(title)     
+        print("=======================================================================")
+        system_menu = '''
+        1. Add new client to the system
+        2. Search clients by name and display their information
+        3. Delete client from the system
+        4. Edit the client's information 
+        5. Display all the clients on the system (Option to select client Type)    
+        6. Exit the system
+        '''     
+        print(f"{Fore.GREEN}{system_menu}")
+        print("========================================================================")
+        option = input(f"{Fore.MAGENTA}Please enter the number between 1 - 6 to run your choice:")
         # Check if user entered the correct value as required
         if option not in ["1","2","3","4","5","6"]:
             print(f"{Back.RED}{Fore.WHITE}Not a valid input, please try again!\n")
@@ -308,34 +304,21 @@ def operation (worksheet):
             if option == "1":
                 add_client(worksheet)
             elif option == "2":
-                delete_client(worksheet)
-            elif option == "3":
-                update_client(worksheet)
-            elif option == "4":
-                get_all_clients(worksheet)
-            elif option == "5":
                 search_clients(worksheet)
+            elif option == "3":
+                delete_client(worksheet)
+            elif option == "4":
+                update_client(worksheet)
+            elif option == "5":
+                get_all_clients(worksheet)            
             elif option == "6":
                 # Exit the operation system
-                print("Thank you for using the clients management system")
+                print(f"{Fore.MAGENTA}Thank you for using the clients management system! Hope to See you soon!")
                 break
             
 def main ():
-    """Display the menu of the system and run programm according to users choice"""
-    clients = SHEET.worksheet('clients')
-    title = pyfiglet.figlet_format("Clients Management System", font="doom", justify="center")
-    print(title)     
-    print("=======================================================================")
-    system_menu = '''
-    1. Add new client to the system
-    2. Delete client from the system
-    3. Edit the client's information 
-    4. Display all the clients on the system (Option to select client Type)
-    5. Search clients by name and display their information
-    6. Exit the system
-    '''     
-    print(system_menu.center(100))
-    print("========================================================================")
+    """Run programm according to users' choices"""    
+    clients = SHEET.worksheet('clients')    
     operation(clients)
 
 if __name__ == "__main__":
